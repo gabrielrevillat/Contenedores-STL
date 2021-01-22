93996fa118fe77c31a02525c812a15a2e2af6ede
Gabriel Revillat <gabrielrevillat@yahoo.es>
2021-01-21 23:42:15 -0600

    Comenzando archivo my_deque.h

---
+++ Contenedores/deque/my_deque.h
@@ -0,0 +1,25 @@
+#ifndef MY_DEQUE_H
+#define MY_DEQUE_H
+
+#include "../algorithm/my_algorithm.h"
+
+#include <utility>
+
+namespace mySTL
+{
+
+#ifndef MY_DEQUE_CHUNK_SIZE
+#define MY_DEQUE_CHUNK_SIZE 512
+#endif
+
+	/**
+	 * 
+	 */
+	constexpr inline std::size_t my_deque_chunk_size(std::size_t size)
+	{
+
+	}
+
+}
+
+#endif /* MY_DEQUE_H */
